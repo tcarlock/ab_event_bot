67c49e6487b5a2bc36881a2ae74573d497fbe48e
Tim Carlock <timothy.carlock@gmail.com>
2013-07-24 23:55:28 -0700

    Removed test data from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,15 +9,3 @@ EventSource.create(
   ticket_url_selector: ".views-field-path a",
   disabled: false
 )
-
-EventSource.create(
-  title: "SF Jazzie",
-  region: "San Francisco, CA",
-  url: "http://www.sfjazz.org/events-calendar",
-  event_item_selector: ".view-sfj-page-body-event-seriesview .views-table tbody tr", 
-  title_link_selector: ".views-field-field-excerpttitle-text-value a", 
-  location_selector: ".views-field-field-excerpttitle-text-value-1 p", 
-  date_selector: ".date-display-single",
-  ticket_url_selector: ".views-field-path a",
-  disabled: false
-)
