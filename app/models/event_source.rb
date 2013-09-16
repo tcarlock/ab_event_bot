c708da819730b56fa33cbe5bbc38a13dae292ab8
Tim Carlock <timothy.carlock@gmail.com>
2013-09-15 18:05:00 -0700

    Patching model to reset store attr

--- app/models/event_source.rb
+++ app/models/event_source.rb
@@ -1,14 +1,14 @@
 class EventSource < ActiveRecord::Base
   before_create :set_scrape_date
-  # after_initialize :set_url_param_defaults
+  after_initialize :set_url_param_defaults
   before_validation :add_url_protocol
 
   URL_PARAMS = {
     PAGE_NUMBER: 5
   }
 
-  attr_accessible :venue_id, :title, :region, :url, :url_params, :last_scraped, :event_link_selector, :title_selector, :description_selector, :location_selector, :date_selector, :time_selector, :ticket_url_selector, :disabled, :event_item_selector, :title_link_selector
-  store :css_mappers, accessors: [:event_link_selector, :title_selector, :description_selector, :location_selector, :date_selector, :time_selector, :ticket_url_selector, :event_item_selector, :title_link_selector]
+  attr_accessible :venue_id, :title, :region, :url, :url_params, :last_scraped, :event_link_selector, :title_selector, :description_selector, :location_selector, :date_selector, :time_selector, :ticket_url_selector, :disabled
+  store :css_mappers, accessors: [:event_link_selector, :title_selector, :description_selector, :location_selector, :date_selector, :time_selector, :ticket_url_selector]
   serialize :url_params, HashWithIndifferentAccess
 
   validates :title, presence: :true
@@ -35,9 +35,9 @@ class EventSource < ActiveRecord::Base
     end
   end
 
-  # def set_url_param_defaults
-  #   URL_PARAMS.each do |key, value|
-  #     self.url_params[key] = value if self.url_params[key].nil?
-  #   end
-  # end
+  def set_url_param_defaults
+    URL_PARAMS.each do |key, value|
+      self.url_params[key] = value if self.url_params[key].nil?
+    end
+  end
 end
